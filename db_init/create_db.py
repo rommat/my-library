829995cfa3965657342556d8174dae0c5e289df8
Roman Matviichuk <roman.matviichuk_rft@thomsonreuters.com>
2021-10-06 01:23:45 +0300

    Add creation of tables 	modified:   db_init/create_db.py

--- db_init/create_db.py
+++ db_init/create_db.py
@@ -6,7 +6,6 @@
 
 from pathlib import Path
 import sqlite3
-# from sqlite3 import Error
 
 
 DB_FILE = Path(__file__).parents[1] / 'db_file' / 'mylibrary.db'
@@ -23,6 +22,19 @@ def create_connection(db_file):
         return err
 
 
+def create_table(db_conn, sql_stmt):
+    """
+    Create a table from the SQL statement
+    :param db_conn: Connection object
+    :param sql_stmt: a CREATE TABLE statement
+    """
+    try:
+        cursor = db_conn.cursor()
+        cursor.execute(sql_stmt)
+    except sqlite3.Error as err:
+        return err
+
+
 if __name__ == '__main__':
     # create a database connection
     conn = create_connection(DB_FILE)
@@ -31,5 +43,50 @@ if __name__ == '__main__':
         print("DB was NOT created.")
         print(conn)
 
-    if isinstance(conn, sqlite3.Connection):
+    elif isinstance(conn, sqlite3.Connection):
         print("DB was created successfully.")
+
+        # create tables
+        cr_tab_stmts = {
+            'books': """
+                CREATE TABLE IF NOT EXISTS books (
+                    book_id integer PRIMARY KEY,
+                    title text NOT NULL,
+                    description text,
+                    isbn text, 
+                    original_language text,
+                    release_date text
+                );
+            """,
+            'authors': """
+                CREATE TABLE IF NOT EXISTS authors (
+                    author_id integer PRIMARY KEY,
+                    first_name text NOT NULL,
+                    last_name text NOT NULL,
+                    birthday text,
+                    death_date text 
+                );
+            """,
+            'writtenby': """
+                CREATE TABLE IF NOT EXISTS writtenby (
+                    record_id integer PRIMARY KEY,
+                    author_id integer NOT NULL,
+                    book_id integer NOT NULL
+                );
+            """
+        }
+        errors = {}
+        for tab in cr_tab_stmts:
+            msg = create_table(conn, cr_tab_stmts[tab])
+            if msg:
+                errors[tab] = msg
+
+        # close the database connection
+        conn.close()
+
+        if errors:
+            print(f"{len(errors)} tables were NOT created.")
+            for tab in errors:
+                print(f"{tab}: {errors[tab]}")
+        else:
+            print("All tables were created successfully.")
